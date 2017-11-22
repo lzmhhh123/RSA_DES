ae82520a9a62d11c5c0a6337400c9c335714693e
lzmhhh123 <lzmhhh123@gmail.com>
2017-11-22 14:05:59 +0800

    add cipherBlockMode

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"ISpj1-2/rsa"
+	"ISpj1-2/myrsa"
+	"crypto/cipher"
+	"crypto/des"
+	"encoding/binary"
 	"fmt"
 )
 
+func ReadDesKey() []byte {
+	fmt.Println("Please enter the DES key by a 64bits integer: ")
+	var desNumber uint64
+	fmt.Scanln(&desNumber)
+	desKey := make([]byte, 8)
+	binary.LittleEndian.PutUint64(desKey, uint64(desNumber))
+	return desKey
+}
+
 func main() {
 	n, publicKey, privateKey := myrsa.GenerateRsaKey()
-	fmt.Println(n, publicKey, privateKey)
+	fmt.Println("RSA public key: (", n, ", ", publicKey, ").")
+	fmt.Println("RSA private key: (", n, ", ", privateKey, ").")
+	desKey := ReadDesKey()
+	cipherBlock, err := des.NewCipher(desKey)
+	if err != nil {
+		panic(err)
+	}
+	cipherBlockMode := cipher.NewCBCDecrypter(cipherBlock, make([]byte, 8))
+
 }
--- myrsa/main.go
+++ myrsa/main.go
@@ -9,9 +9,9 @@ import (
 /*
   Judge the number whether prime
 */
-func IsPrime(number int64) bool {
+func IsPrime(number uint64) bool {
 	lim := math.Sqrt(float64(number))
-	for i := int64(2); float64(i) <= lim; i++ {
+	for i := uint64(2); float64(i) <= lim; i++ {
 		if number%i == 0 {
 			return false
 		}
@@ -23,19 +23,22 @@ func IsPrime(number int64) bool {
   Generate prime randly
 */
 func GeneratePrime() *big.Int {
-	prime := rand.Int63()
+	prime := rand.Uint64()
 	for !IsPrime(prime) {
-		prime = rand.Int63()
+		prime = rand.Uint64()
 	}
-	ret := big.NewInt(prime)
+	ret := big.NewInt(int64(prime))
 	return ret
 }
 
+/*
+	exgcd algorithm
+*/
 func exgcd(a *big.Int, b *big.Int) (x *big.Int, y *big.Int) {
-	if b == big.NewInt(0) {
+	tmp := big.NewInt(0)
+	if b.Cmp(big.NewInt(0)) == 0 {
 		return big.NewInt(1), big.NewInt(0)
 	}
-	var tmp *big.Int
 	tx, ty := exgcd(b, tmp.Mod(a, b))
 	x = ty
 	y = tmp.Sub(tx, tmp.Mul(tmp.Div(a, b), ty))
@@ -47,10 +50,13 @@ func exgcd(a *big.Int, b *big.Int) (x *big.Int, y *big.Int) {
 */
 func GenerateRsaKey() (n *big.Int, publicKey *big.Int, privateKey *big.Int) {
 	p, q := GeneratePrime(), GeneratePrime()
-	var tmp *big.Int
+	tmp := big.NewInt(0)
 	n = tmp.Mul(p, q)
 	fiN := tmp.Mul(tmp.Sub(p, big.NewInt(1)), tmp.Sub(q, big.NewInt(1)))
 	e := big.NewInt(65537)
 	d, _ := exgcd(e, fiN)
+	if d.Cmp(big.NewInt(0)) == -1 {
+		d.Add(d, fiN)
+	}
 	return n, e, d
 }
